56721c3f766493f09bc04b380a404a4d83a185f6
Yash Srivastava <yashsrivastava@Yash.local>
2026-07-12 16:53:50 +0530

    Rename project structure

---
+++ models/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from . import asset
+from . import asset_allocation
+from . import asset_category
+from . import asset_transfer
+from . import audit
+from . import booking
+from . import dashboard
+from . import department
+from . import employee
+from . import maintenance
+from . import notification
+from . import report
+from . import setings
---
+++ wizard/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import allocate_asset
+from . import booking_wizards
+from . import maintenance_wizard
+from . import return_assets
